5aab4ba19d4c0e6b4fda32fa1d33b582641a909f
fearless-command-2358 <iuriseara@gmail.com>
2020-05-27 19:53:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,7 +5,7 @@ def start_game(player1, player2)
   return greeting
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   compare = "#{player1} is better than #{player2}"
   return compare
 end
