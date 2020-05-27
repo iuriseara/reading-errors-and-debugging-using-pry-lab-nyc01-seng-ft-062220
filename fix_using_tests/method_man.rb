6ab936aa63ec87443c498a59828b6d36353f85ae
fearless-command-2358 <iuriseara@gmail.com>
2020-05-27 19:42:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,11 +3,10 @@ require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
+  binding.pry
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
   binding.pry
 end
-
-start_game("Rocky", "Bullwinkle")
