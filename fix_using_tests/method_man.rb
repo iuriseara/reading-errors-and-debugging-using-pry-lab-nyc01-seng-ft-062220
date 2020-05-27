5629a441c6172774d28137d5a2ddce37f83eec3e
fearless-command-2358 <iuriseara@gmail.com>
2020-05-27 19:54:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,7 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  greeting = "Hello #{player1} & #{player2}"
-  return greeting
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
