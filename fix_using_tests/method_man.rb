f8843cb5544b1bd7005b81a5ea973df90844c310
fearless-command-2358 <iuriseara@gmail.com>
2020-05-27 19:52:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,10 @@
 # don't forget to add: require 'pry'
-require "pry"
 
 def start_game(player1, player2)
   greeting = "Hello #{player1} & #{player2}"
   return greeting
-  binding.pry
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
-  binding.pry
 end
